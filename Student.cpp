6f39f0914ba940a5e467a0a946f08846d33b9e9c
Kalpa punchihewa <99349489+IT21239434@users.noreply.github.com>
2022-03-16 05:45:07 +0000

    completed

--- Student.cpp
+++ Student.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 
 // Assign studentId and name
-Student::assignDetails() {
-    
+Student::assignDetails(int id , char sname[]){
+  studentID = id ;
+  strcpy(name , sname);
 }
 
 // Display StudentId and Name
 Student::display() {
-  
+  cout << "The student id : " << endl;
+  cout << "The name of the student : " << endl;
 }
